efb921a3e245a6965a68c16e3f134123d882f75b
Nickolas Ciochina <nickolas.ciochina@student.graafschapcollege.nl>
2024-09-17 10:56:46 +0200

    Refresh multiple recipes bug fixed

--- recipes.js
+++ recipes.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     $('#searchButton').on('click', function () {
         $.ajax({
-            url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=c25e2d349ba842ee8186ded1ff30b942`
+            url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=3cc602c4888c404f97f95f614a29588e`
         }).done(function (data) {
-            data.results.forEach(recipe => {
-                const searchResults = document.querySelector('.searchResults');
+            const searchResults = document.querySelector('.searchResults');
+            
+            // Clear previous search results
+            searchResults.innerHTML = '';
 
+            data.results.forEach(recipe => {
                 const newItem = document.createElement('div');
                 newItem.classList.add('recipe');
                 newItem.innerHTML = `<h3>${recipe.title}</h3><a href="ingredients.html?id=${recipe.id}"><img src=${recipe.image} /></a>`;
